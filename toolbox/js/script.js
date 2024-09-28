a5797de21389f39474dd1a59e57f277f6c05bbee
Dhruvin-04 <chudasamadhruvin28@gmail.com>
2024-09-28 22:53:20 +0530

    animated border, mikels work

--- toolbox/js/script.js
+++ toolbox/js/script.js
@@ -6,8 +6,26 @@ const suggBox = searchWrapper.querySelector(".autocom-box");
 // Example suggestions array with relative paths
 const suggestions = [
     { name: "PDF to Word", link: "coming soon/index.html" },
+    { name: "PDF to PP", link: "coming soon/index.html"}, 
+    { name: "Word to PDF", link: "coming soon/index.html" }, 
+    { name: "Compress PDF", link: "coming soon/index.html" }, 
+    { name: "PP to PDF", link: "coming soon/index.html" }, 
+    { name: "Word Counter", link: "tools/wordcount/index.html" }, 
+    { name: "Font Styles", link: "tools/font styles/index.html" }, 
+    { name: "Grammar Checker", link: "tools/grammarly/index.html" }, 
+    { name: "Case Changer", link: "tools/cases/index.html" }, 
+    { name: "Whatsapp Spammer", link: "tools/whatsapp spammer/index.html" }, 
+    { name: "Password Generator", link: "tools/password genarator/index.html" }, 
     { name: "EMI Calculator", link: "tools/emi calculator/index.html" },
-    { name: "SIP Calculator", link: "tools/sip calculator/index.html" } 
+    { name: "SIP Calculator", link: "tools/sip calculator/index.html" }, 
+    { name: "Currency Coverter", link: "tools/currencyconvertor/index.html" }, 
+    { name: "Loan Calculator", link: "tools/loan calculator/index.html" }, 
+    { name: "Income Tax Calculator", link: "tools/income tax calculator/index.html" }, 
+    { name: "Body Mass Index", link: "tools/BMI calculator/index.html" }, 
+    { name: "Pregnancy Due Date", link: "tools/pregnancy due date cal/index.html" }, 
+    { name: "Age Calculator", link: "tools/age calculator/index.html" }, 
+    { name: "Fat Percentage Calculator", link: "tools/fat percentage calculator/index.html" }, 
+    { name: "Calorie Maintenance", link: "tools/calorie maintenance/index.html" }
 ];
 
 // Handling input and showing suggestions
